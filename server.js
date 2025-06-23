9ce77db78dd7410e0b7622c2c53db84964891b44
ZaktRay <zakt7737@gmail.com>
2025-06-23 20:44:06 +0530

    m

--- server.js
+++ server.js
@@ -17,9 +17,8 @@ connectDB();
 const app = express();
 
 app.use(cors({
-    origin: '*',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
 }))
 
